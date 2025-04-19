c0f72dbc833f8c6787c100ad59691a4bf8db8fc8
enzo ipanaque <eipanaquep@pucp.edu.pe>
2025-04-18 22:49:40 -0500

    Recepción de audios

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 from dotenv import load_dotenv
 from datetime import datetime
 import json
+import tempfile
 
 load_dotenv()
 make_webook_url = os.getenv("WEBHOOK_MAKE_URL")
@@ -20,23 +21,44 @@ def home():
 def webhook():
     data = request.form
     mensaje = data.get("Body", "")
+    media_url = data.get("MediaUrl0")
+    media_type = data.get("MediaContentType0", "")
     
-    prompt = f"""
-    Extrae los siguientes campos desde este mensaje de WhatsApp y estructura la respuesta en JSON:
-
-    - descripcion
-    - monto
-    - categoria
-    - medio (Signature o Yape)
-    - moneda (PEN o USD)
-    - tipo (Gasto o Ingreso)
-
-    Ejemplo de mensaje: "{mensaje}"
-
-    Respondeme solo con el json, sin texto adicional. Si no encuentras un campo, infierelo.
-    """
     try:
         client = OpenAI()
+
+        if media_url and "audio" in media_type:
+            audio_response = requests.get(media_url)
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".ogg") as tmp_audio:
+                    tmp_audio.write(audio_response.content)
+                    tmp_audio_path = tmp_audio.name
+            
+            with open(tmp_audio_path, "rb") as audio_file:
+                    transcript = client.audio.transcriptions.create(
+                        model="whisper-1",
+                        file=audio_file,
+                        response_format="text"
+                    )
+                    mensaje_texto = transcript.strip()
+
+        if mensaje_texto:
+            mensaje = mensaje_texto
+
+        prompt = f"""
+        Extrae los siguientes campos desde este mensaje de WhatsApp y estructura la respuesta en JSON:
+
+        - descripcion
+        - monto
+        - categoria
+        - medio (Signature o Yape)
+        - moneda (PEN o USD)
+        - tipo (Gasto o Ingreso)
+
+        Ejemplo de mensaje: "{mensaje}"
+
+        Respondeme solo con el json, sin texto adicional. Si no encuentras un campo, infierelo.
+        """
+
         response = client.responses.create(
             model = "gpt-4.1",
             input = prompt
