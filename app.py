36ecd4d7263ae043856f091b618b9e9adc4e6573
enzo <enzo.ip.98@gmail.com>
2025-07-21 14:58:28 -0500

    test new number flow

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, request, jsonify
 import requests
-from openai import OpenAI
 import os
-from dotenv import load_dotenv
-from datetime import datetime
 import json
 import tempfile
+import pickle
+from dotenv import load_dotenv
+from datetime import datetime
+from flask import Flask, request, jsonify
+from openai import OpenAI
 from twilio.rest import Client
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 
 load_dotenv()
 make_webook_url = os.getenv("WEBHOOK_MAKE_URL")
@@ -17,6 +21,46 @@ DESTINATION_NUMBER = os.environ.get('DESTINATION_NUMBER')
 
 app = Flask(__name__)
 
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+TOKEN_PATH = os.getenv('TOKEN_PICKLE', 'token.pickle')
+CLIENT_SECRET_PATH = os.getenv('CLIENT_SECRET', 'credentials.json')
+
+def get_credentials():
+    creds = None
+    if os.path.exists(TOKEN_PATH):
+        with open(TOKEN_PATH, 'rb') as token:
+            creds = pickle.load(token)
+    print("Credenciales cargadas")
+    return creds
+
+def response(mensaje: str, DESTINATION_NUMBER: str):
+    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+    try:
+        sent = client.messages.create(
+            body=mensaje,
+            from_=TWILIO_WHATSAPP_NUMBER,
+            to=DESTINATION_NUMBER
+        )
+        return jsonify({"status": "Mensaje enviado", "sid": sent.sid}), 200
+    except Exception as e:
+        import traceback
+        print("Error en /response:", traceback.format_exc())
+        return jsonify({"error": str(e)}), 500
+
+creds = get_credentials()
+sheets_service = build('sheets', 'v4', credentials=creds)
+SPREADSHEET_ID = os.getenv('USUARIOS_ID')
+SHEET_NAME = os.getenv('SHEET_NAME')
+SEARCH_COLUMN = "phone"
+RANGE_ALL = f"{SHEET_NAME}!A:G"
+
+mensaje_informacion_usuario = """
+    Bienvenido al Asistente Financiero. Para continuar necesitaré la siguiente información:
+    correo electrónico: (correo de gmail)
+    categorías de gasto: Ejemplo (Servicios, Pareja, Hogar, Comida, Movilidad, Gustos)
+"""
+
+
 
 @app.route('/')
 def home():
@@ -25,93 +69,57 @@ def home():
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.form
-    mensaje = data.get("Body", "")
-    media_url = data.get("MediaUrl0")
-    media_type = data.get("MediaContentType0", "")
-    
-    try:
-        client = OpenAI()
+    numero: str = data.get("From", "").split("+")[-1]
+    mensaje: str = data.get("Body", "")
+    #media_url = data.get("MediaUrl0")
+    #media_type = data.get("MediaContentType0", "")
+    try: 
+        result = sheets_service.spreadsheets().values().get(
+            spreadsheetId=SPREADSHEET_ID,
+            range=RANGE_ALL
+        ).execute()
+        rows = result.get('values', [])
+        if not rows:
+            return jsonify({'error': 'La hoja está vacía'}), 404
+        header: list[str] = rows[0]
+        if SEARCH_COLUMN not in header:
+            return jsonify({'error': f'Columna "{SEARCH_COLUMN}" no encontrada'}), 400
+        idx = header.index(SEARCH_COLUMN)
+        for row in rows[1:]:
+            if row[idx] == numero:
+                mail: str = row[1]
+                status: str = row[2]
+                categories: str = row[3]
+                url_sheet: str = row[4]
+                if status == "inactive":
+                    print(numero," inactivo")
+                    #codigo para llamar a chat gpt preguntando por el la información del usuario usando el mensaje
+                    return jsonify({'status': 'ok', 'message': 'Usuario encontrado, se solicita registro.'}), 200
+                elif status == "active":
+                    print(numero," activo")
+                    #codigo para insertar regitro en la hoja de calculo
+                    return jsonify({'status': 'ok', 'message': 'Se registra informacion del usuario.'}), 200
+        print(numero," no encontrado")
+        insertion_row_number = len(rows) + 1
+        insertion_row = {'values':[[numero,'','inactive','','',datetime.now().strftime('%Y-%m-%d %H:%M:%S')]]}
+        return response(mensaje_informacion_usuario, numero)
+    except:
+        return jsonify({'error': 'Error en el proceso, contactar con administrador'}), 500
 
-        if media_url and "audio" in media_type:
-            audio_response = requests.get(media_url)
-            with tempfile.NamedTemporaryFile(delete=False, suffix=".ogg") as tmp_audio:
-                    tmp_audio.write(audio_response.content)
-                    tmp_audio_path = tmp_audio.name
-            
-            with open(tmp_audio_path, "rb") as audio_file:
-                    transcript = client.audio.transcriptions.create(
-                        model="whisper-1",
-                        file=audio_file,
-                        response_format="text"
-                    )
-                    mensaje_texto = transcript.strip()
-
-            if mensaje_texto:
-                mensaje = mensaje_texto
-
-        prompt = f"""
-        Actúa como un asistente financiero que me va a ayudar a registrar mis ingresos y egresos de manera precisa y estructurada.
-        Analiza el siguiente mensaje de WhatsApp y extrae la información que te solicito a continuación en un JSON.
-
-        - descripcion
-        - monto
-        - categoria (Debe estar en una de las siguientes:Servicios (Pago de internet, Pago de Luz, Pago de telefono, Pago de Servicios de nube), Pareja(almuerzos, salidas y citas con mi pareja), Hogar(compras en supermercados de comida y de cosas de limpieza), Entretenimiento(fiestas y o salidas sin pareja), Movilidad (gasolina y taxis), Paz Mental(regalos a mi mismo o a mi familia))
-        - medio (Signature o Yape, asume Signature si no lo menciono)
-        - moneda (PEN o USD, asume PEN si no lo mencionos)
-        - tipo (Gasto o Ingreso)
-
-        El mensaje es: "{mensaje}"
-
-        Tu respuesta debe ser un json que va a pasar por la librería de json de, sin texto adicional. No inventes información.
-        """
-        
-        schema = {
-            "type": "object",
-            "properties": {
-                "descripcion": {"type": "string"},
-                "monto":       {"type": "string"},   # ← todo string
-                "categoria":   {"type": "string"},
-                "medio":       {"type": "string"},
-                "moneda":      {"type": "string"},
-                "tipo":        {"type": "string"}
-            },
-            "required": ["descripcion", "monto", "categoria",
-                        "medio", "moneda", "tipo"],
-            "additionalProperties": False
-        }
-        response = client.responses.create(
-            model = "gpt-4.1",
-            input = prompt
-        )
-        print(response.output_text)
-        json_data =  response.output_text
-        datos = json.loads(json_data)
-        datos["fecha"] = datetime.now().strftime("%Y-%m-%d")
-        if make_webook_url:
-            requests.post(make_webook_url,json = datos)
-        return jsonify({"status": "ok"}), 200
-
-    except Exception as e:
-        return jsonify({"status": "error", "message": str(e)}), 500
 @app.route('/response',methods=['POST'])
-def response():
-    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+def response_twilio(DESTINATION_NUMBER):
+    
     data = request.get_json()
     message = data.get('mensaje') or data.get('message')  # soporte para ambos keys
 
     if not message:
         return jsonify({"error": "Falta el campo 'mensaje' en el cuerpo del request"}), 400
-
     try:
-        sent = client.messages.create(
-            body=message,
-            from_=TWILIO_WHATSAPP_NUMBER,
-            to=DESTINATION_NUMBER
-        )
-        return jsonify({"status": "Mensaje enviado", "sid": sent.sid}), 200
+        response(message, DESTINATION_NUMBER)
     except Exception as e:
         import traceback
-        print("❌ Error en /response:", traceback.format_exc())
+        print("Error en /response:", traceback.format_exc())
         return jsonify({"error": str(e)}), 500
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
